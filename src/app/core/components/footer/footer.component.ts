feba1c697c822c14952f81e9dfa20500d262e274
marwanabubakr <dev@marobakr>
2024-04-22 13:42:37 +0200

    refactor (@core/footer): change bg color

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import {} from '@angular/material/icon';
-import { FlexLayoutModule } from '@angular/flex-layout';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, FlexLayoutModule],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  primary = '#673ab7';
   cuurentData: Date = new Date();
 }
